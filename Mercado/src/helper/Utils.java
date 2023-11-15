38286c1689692b1ddce0bd35c20b8c1e49f73743
ALEXANDRE LORENA <alexandre.lorena@gmail.com>
2023-11-15 19:15:38 -0300

    atualizações projetos em java

--- Mercado/src/helper/Utils.java
+++ Mercado/src/helper/Utils.java
@@ -1,2 +1,42 @@
-package helper;public class Utils {
+package helper;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class Utils {
+
+    static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    static NumberFormat nf = new DecimalFormat("R$ #,##0.00",
+            new DecimalFormatSymbols(new Locale("pt", "BR")));
+
+    public static String dateParaString(Date data) {
+        return Utils.sdf.format(data);
+    }
+
+    public static String doubleParaString(Double valor) {
+        return Utils.nf.format(valor);
+    }
+
+    public static Double stringParaDouble(String valor) {
+        try {
+            return (Double)Utils.nf.parse(valor);
+        }catch(ParseException e) {
+            return null;
+        }
+    }
+
+    public static void pausar(int segundos) {
+        try {
+            TimeUnit.SECONDS.sleep(segundos);
+        }catch(InterruptedException e) {
+            System.out.println("Erro ao pausar por " + segundos + " segundos.");
+        }
+    }
+
 }
--- projetoGame/GamePanel.java
+++ projetoGame/GamePanel.java
@@ -1,9 +1,14 @@
+// -*- coding: UTF-8 -*-
 package projetoGame;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
@@ -19,15 +24,16 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingWorker;
 
+
+@SuppressWarnings("serial")
 public class GamePanel extends JPanel {
-    private static final long serialVersionUID = 1L; // Número de versão serial
-
     private JTextField respostaField;
     private JButton verificarButton;
     private JLabel operacaoLabel;
     private JLabel pontosLabel;
-    private JButton githubButton; // Novo botão para o GitHub
+    private JButton githubButton;
     private GameLogic gameLogic;
 
     public GamePanel() {
@@ -40,26 +46,50 @@ public class GamePanel extends JPanel {
         add(operacaoLabel);
 
         respostaField = new JTextField(10);
+        respostaField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // Não é necessário implementar keyTyped para este caso
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    processarResposta();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                // Não é necessário implementar keyReleased para este caso
+            }
+        });
         add(respostaField);
 
         verificarButton = new JButton("Verificar");
         verificarButton.addActionListener(new VerificarListener());
+        verificarButton.setForeground(Color.BLUE);
         add(verificarButton);
 
         pontosLabel = new JLabel("Pontos: 0");
         pontosLabel.setHorizontalAlignment(JLabel.CENTER);
+        pontosLabel.setOpaque(true); // Define o componente como opaco
+        pontosLabel.setBackground(Color.yellow);
         add(pontosLabel);
-
-        githubButton = new JButton("Abrir GitHub");
+        
+        githubButton = new JButton("Visite o meu GitHub");
         githubButton.addActionListener(new GithubListener());
-        add(githubButton);
-
+        githubButton.setPreferredSize(new Dimension(150, 10)); // Ajuste a largura e altura conforme necessário
+        githubButton.setBackground(Color.lightGray); // Defina a cor desejada
+        githubButton.setForeground(Color.BLACK); // Define a cor do texto para garantir visibilidade
+        add(githubButton, BorderLayout.SOUTH);
+        
         mostrarNovaOperacao();
     }
 
     @Override
     public Dimension getPreferredSize() {
-        return new Dimension(300, 250); // Ajustei a altura para acomodar o novo botão
+        return new Dimension(300, 250);
     }
 
     private void mostrarNovaOperacao() {
@@ -68,22 +98,51 @@ public class GamePanel extends JPanel {
         respostaField.setText("");
     }
 
+    private void processarResposta() {
+        try {
+            int resposta = Integer.parseInt(respostaField.getText());
+            if (gameLogic.verificarResposta(resposta)) {
+                gameLogic.incrementarPontos();
+                pontosLabel.setText("Pontos: " + gameLogic.getPontos());
+                playAcertoSound();
+            } else {
+                playErroSound();
+            }
+            mostrarNovaOperacao();
+        } catch (NumberFormatException ex) {
+            new SwingWorker<Void, Void>() {
+                @Override
+                protected Void doInBackground() throws Exception {
+                    playInvalidNumberSound();
+                    return null;
+                }
+
+                @Override
+                protected void done() {
+                    JOptionPane.showMessageDialog(GamePanel.this, "Digite um número válido!");
+                }
+            }.execute();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void playAcertoSound() {
+        playSound("projetoGame/success.wav");
+    }
+
+    private void playErroSound() {
+        playSound("projetoGame/error.wav");
+    }
+
+    private void playInvalidNumberSound() {
+        playSound("projetoGame/notificacao.wav");
+    }
+
     private class VerificarListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            try {
-                int resposta = Integer.parseInt(respostaField.getText());
-                if (gameLogic.verificarResposta(resposta)) {
-                    gameLogic.incrementarPontos();
-                    pontosLabel.setText("Pontos: " + gameLogic.getPontos());
-                    playAcertoSound(); // Toca o som de acerto
-                } else {
-                    playErroSound(); // Toca o som de erro
-                }
-                mostrarNovaOperacao();
-            } catch (NumberFormatException ex) {
-                JOptionPane.showMessageDialog(GamePanel.this, "Informe um número válido");
-            }
+            processarResposta();
         }
     }
 
@@ -103,14 +162,6 @@ public class GamePanel extends JPanel {
         }
     }
 
-    private void playErroSound() {
-        playSound("projetoGame/error.wav");
-    }
-
-    private void playAcertoSound() {
-        playSound("projetoGame/success.wav");
-    }
-
     private void playSound(String filePath) {
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(filePath));
@@ -122,3 +173,5 @@ public class GamePanel extends JPanel {
         }
     }
 }
+
+
